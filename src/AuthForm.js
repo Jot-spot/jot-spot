8550c282037947492ee70881f8073c1a147345cb
Mohamed-Abass2287 <aminabass987@gmail.com>
2025-06-24 15:31:30 +0300

    Added form submission handler with login and token storage

--- src/AuthForm.js
+++ src/AuthForm.js
@@ -6,3 +6,23 @@ function AuthForm ({setToken}){
     const [password, setPassword] = useState('');
     
 }
+function handleSubmit(e) {
+    e.preventDefault();
+    const route = isLogin ? '/login' : '/register';
+    fetch(route, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password })
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.access_token) {
+          localStorage.setItem('token', data.access_token);
+          setToken(data.access_token);
+        } else if (data.message) {
+          alert(data.message);
+        } else {
+          alert("Invalid credentials or username taken.");
+        }
+      });
+  }
