bc629f69fc4e0a8228f3ee0226a6b8a0a22840d8
Timothy <timothy.waweru1@student.moringaschool.com>
2025-06-24 15:21:52 +0300

    Added useefect to fetch user data when tooken is available

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,16 @@ function App() {
   const [token, setToken] = useState(localStorage.getItem('token'));
   const [search, setSearch] = useState('');
 
+
+  useEffect(() => {
+    if (token) {
+      fetch('/notes', {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+        .then(res => res.json())
+        .then(data => setNotes(data));
+    }
+  }, [token]);
   return (
     <div>
 
