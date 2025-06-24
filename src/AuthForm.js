c06e3b6491787bf9548514fc778f9880cddf3dca
Mohamed-Abass2287 <aminabass987@gmail.com>
2025-06-24 15:15:40 +0300

    Initialized AuthForm component with login state and input fields

--- src/AuthForm.js
+++ src/AuthForm.js
@@ -1,1 +1,8 @@
-//client/src/AuthForm.js
+import React, { useState } from 'react';
+
+function AuthForm ({setToken}){
+    const [isLogin, setIsLogin] = useState(true);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    
+}
