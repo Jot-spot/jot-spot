6576c26c757e905645ce536e32a05b4e6f54cf69
Timothy <timothy.waweru1@student.moringaschool.com>
2025-06-24 14:46:34 +0300

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,18 @@ import Profile from "./components/Profile"
 import Notes from "./components/Notes"
 
 function App() {
-  
+  const isLoggedIn = () => !!localStorage.getItem("token")
   return (
     <>
+    <Router>
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </Router>
     </>
   )
 }
