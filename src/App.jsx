ba0659c2ca2e9fddfa25d51f38123f243b41684e
Timothy <timothy.waweru1@student.moringaschool.com>
2025-06-24 15:07:53 +0300

    Initialized app compo with notes,auth and form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-
+import NoteForm from "./NoteForm"
+import NoteList from "./NoteList"
+import AuthForm from "./AuthForm"
 function App() {
-  
-
   return (
-    <>
-    </>
+    <div>
+
+    </div>
   )
 }
 
