177c1420d73cfc7f0bfff58d9eb7eccc3827c18f
Nelson <nelson.atiang@student.moringaschool.com>
2025-06-25 03:07:57 +0300

    Added  state to NoteItem to enable editing title, content, and tags

--- src/NoteItem.js
+++ src/NoteItem.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown';
 
 
 function NoteItem({note,onUpdate,onDelete,token}) {
+    const [isEditing,setIsEditing]=useState(false);
+    const[title,setTitle]=useState(note.title);
+    const[content,setContent]=useState(note.content);
+    const[tags,setTags]=useState(note.tags || '');
+
+    
   return (
     <div>NoteItem</div>
   )
