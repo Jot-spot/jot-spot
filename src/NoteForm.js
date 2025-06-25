5debe4d0d557dbeccb30953a6978bf10a16badcf
kennedy218218 <kennedy.mwai@student.moringaschool.com>
2025-06-25 18:42:14 +0300

    Initialized NoteForm component with state for title, content, and tags

---
+++ src/NoteForm.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+function NoteForm({ onAddNote, token }) {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [tags, setTags] = useState('');
+}
