ec3d294af9bfde6fbf57387dd91a161eed35ecc6
Nelson <nelson.atiang@student.moringaschool.com>
2025-06-25 03:01:21 +0300

    Added NoteItem component  with props for note, update, delete, and token

---
+++ src/NoteItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactMarkdown from 'react-markdown';
+
+
+function NoteItem({note,onUpdate,onDelete,token}) {
+  return (
+    <div>NoteItem</div>
+  )
+}
+
+export default NoteItem
